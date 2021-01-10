42229ab9d92ed5928e5bff9ce3a8393f29af4471
IlScottlI <scotteverettjohnson84@gmail.com>
2021-01-10 06:35:16 -0500

    First React With JSX

---
+++ Fundamentals/006_First_React_With_JSX/app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Hello Dojo!</h1>
+      </header>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li> Learn React </li>
+        <li> Climb Mt Everest </li>
+        <li> Run a marathon </li>
+        <li> Feed the dogs </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
